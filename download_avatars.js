0912670d17d9902ae1dd5a62ef493fb971bc2b20
Julia <bartleyjulia@gmail.com>
2018-01-23 20:57:28 +0000

    Added comments

--- download_avatars.js
+++ download_avatars.js
@@ -5,11 +5,17 @@ var fs = require('fs');
 var repoOwner = process.argv[2];
 var repoName = process.argv[3];
 
+// Creates a callback function for lack of arguments passed to function
+
 function noEmptyVariables() {
   console.log('Danger Will Robinson! E.g. Please enter a valid repoOwner and repoName.');
 }
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+
+// Formats url, parses JSON and receives url information
+
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
@@ -24,6 +30,8 @@ function getRepoContributors(repoOwner, repoName, cb) {
   });
 }
 
+//Function for downloading image by URL
+
 function downloadImageByURL(url, filePath) {
   request.get(url)
     .on('error', function (err) {
@@ -35,19 +43,23 @@ function downloadImageByURL(url, filePath) {
     .pipe(fs.createWriteStream(filePath));
 }
 
+//Requires arguments, else invokes function noEmptyVariables
+
 if (!repoOwner || !repoName) {
   noEmptyVariables();
 }
 
-
+// Invokes getRepo function
 
 getRepoContributors(repoOwner, repoName, function(err, result) {
   console.log("Errors:", err);
   var avatarsFolder = './avatars';
+  //  Creates file folder for photos if non exsistent
   if (!fs.existsSync(avatarsFolder)){
     fs.mkdirSync(avatarsFolder);
   }
   result.forEach( function(userinfo){
+    // Invokes downloadImage function and assigns login name to file, assigns files to Avatars folder
     downloadImageByURL(userinfo.avatar_url, avatarsFolder + '/' + userinfo.login + '.jpg');
   });
 });
