9a8386dba46d77653f294ead1acfb608e93a6d5f
Julia <bartleyjulia@gmail.com>
2018-01-23 04:36:40 +0000

    Printing each avatar_url

--- download_avatars.js
+++ download_avatars.js
@@ -1,20 +1,26 @@
 var request = require('request');
-var secret = require('secrets');
+var secret = require('./secrets');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
-  headers: {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
       'User-Agent': 'request'
-
     }
-  request(url, function(err, res, body) {
-    cb(err, body);
+  };
+   request(options, function(err, res, body) {
+  var obj = JSON.parse(body);
+    cb(err, obj);
   });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
+  result.forEach( function(userinfo){
+
+    console.log(userinfo.avatar_url);
+  })
 });
